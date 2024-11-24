d742d3a30400498a219046fe7e31399afd9c1076
HiTech0926 <2025138508@qq.com>
2024-11-24 19:40:43 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'HiTech0926 网站状态',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm798082493-e9e23ea14a736df10d78b973',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '主页',
+      url: 'https://hitech0926.com/'
+    },
+    {
+      text: '博客',
+      url: 'https://blog.hitech0926.com/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/HiTech0926'
+    },
+  ],
+};
